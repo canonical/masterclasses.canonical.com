41822b1a855811a2830445e314f51df4165bd349
Aaryan Porwal <aaryan.porwal@canonical.com>
2024-04-27 18:05:23 +0530

    Loading thumbnails lazily and cleanup

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 
 from alembic import context
 from models.base import Base
+
 # https://stackoverflow.com/a/62925227
 from models.PreviousSession import PreviousSession
 from models.UpcomingSession import UpcomingSession
@@ -71,9 +72,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- webapp/masterclasses.py
+++ webapp/masterclasses.py
@@ -21,20 +21,6 @@ masterclasses = flask.Blueprint(
 )
 
 
-# Parse the id from a google url
-# https://drive.google.com/open?id=15cy6HFRkzgidDQ1Ff1wBDP-TVihfg_15
-# https://drive.google.com/file/d/1YkOAdSwmAHmddwhQ2l0sbEQyflbkhQ5L/view
-# https://drive.google.com/file/d/1nCbTZtX380e6lD1kkQ6BjTA0aAknZJGY/view?usp=drivesdk
-# https://drive.google.com/file/d/1TQLA53RPhf20Q9BeqMN6e1DFb_kPsF91/view?usp=drive_web
-def get_id(video_link):
-    if video_link:
-        if "id=" in video_link:
-            return video_link.split("id=")[1]
-        elif "file/d/" in video_link:
-            return video_link.split("file/d/")[1].split("/view")[0]
-    return ""
-
-
 @masterclasses.route("/")
 def index():
     previous_sessions = get_previous_sessions()
@@ -50,12 +36,18 @@ def index():
         tags=tags,
     )
 
+
 def format_date(date):
     # change the date format, like 23 May 2024
     return date.strftime("%d %b %Y")
 
+
 def get_upcoming_sessions():
-    session = db_session.query(UpcomingSession).order_by(nullslast(UpcomingSession.date.desc())).all()
+    session = (
+        db_session.query(UpcomingSession)
+        .order_by(nullslast(UpcomingSession.date.desc()))
+        .all()
+    )
     for i in range(len(session)):
         if session[i].date:
             session[i].date = format_date(session[i].date)
@@ -63,7 +55,9 @@ def get_upcoming_sessions():
 
 
 def get_previous_sessions():
-    session = db_session.query(PreviousSession).order_by(PreviousSession.date.desc()).all()
+    session = (
+        db_session.query(PreviousSession).order_by(PreviousSession.date.desc()).all()
+    )
     for i in range(len(session)):
         session[i].date = format_date(session[i].date)
     return session
@@ -75,7 +69,12 @@ def get_sprint_sessions():
 
 
 def get_tags():
-    tag_counts = db_session.query(PreviousSession.tags, func.count(PreviousSession.tags)).filter(PreviousSession.tags.isnot(None)).group_by(PreviousSession.tags).all()
+    tag_counts = (
+        db_session.query(PreviousSession.tags, func.count(PreviousSession.tags))
+        .filter(PreviousSession.tags.isnot(None))
+        .group_by(PreviousSession.tags)
+        .all()
+    )
     tags = {}
     for tag, count in tag_counts:
         for t in tag.split(","):
