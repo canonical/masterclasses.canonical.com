960ec6466ca8315899538639314d19110ac8e579
Aaryan Porwal <aaryan.porwal@canonical.com>
2024-05-03 00:59:21 +0530

    Add new Admin UI with authentication

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,21 @@
+import os
 import flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from canonicalwebteam.flask_base.app import FlaskBase
+
+from flask_admin import Admin
+from flask_admin import AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+from models.PreviousSession import PreviousSession
+from models.UpcomingSession import UpcomingSession
+from models.SprintSession import SprintSession
+
+
+db_engine = create_engine(os.getenv("DATABASE_URL"))
+db_session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+)
 
 from webapp.masterclasses import masterclasses
 from webapp.sso import init_sso
@@ -16,6 +32,20 @@ app = FlaskBase(
 init_sso(app)
 
 app.register_blueprint(masterclasses, url_prefix="/")
+
+
+class RestrictedModelView(ModelView):
+    # Admin view is accesible to Web&Design team
+    def is_accessible(self):
+        return os.getenv("OPENID_LAUNCHPAD_TEAM") == "canonical-content-people"
+
+
+admin = Admin(app)
+admin.add_views(
+    RestrictedModelView(PreviousSession, db_session),
+    RestrictedModelView(UpcomingSession, db_session),
+    RestrictedModelView(SprintSession, db_session),
+)
 
 
 @app.route("/")
